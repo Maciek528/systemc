bc3463739a0043c61c2ca232cc814f17127a2ee1
maciejj <maciejj@kth.se>
2017-11-16 20:10:36 +0100

    cache made work

--- src/cache/cache.cpp
+++ src/cache/cache.cpp
@@ -5,7 +5,6 @@
 
 using namespace std;
 
-// static const int MEM_SIZE = 512;
 static const int NUM_SETS = 128;
 static const int NUM_LINES = 8;
 static const int LRU_POSITION = NUM_LINES - 1;
@@ -13,419 +12,346 @@ static const int MRU_POSITION = 0;
 
 enum Function
 {
-    FUNC_READ,
-    FUNC_WRITE
+  FUNC_READ,
+  FUNC_WRITE
 };
 
 enum RetCode
 {
-    RET_READ_DONE,
-    RET_WRITE_DONE
+  RET_READ_DONE,
+  RET_WRITE_DONE
 };
-
-// SC_MODULE(Memory)
-// {
-// public:
-//     sc_in<bool>     Port_CLK;
-//     sc_in<Function> Port_Func;
-//     sc_in<int>      Port_Addr;
-//     sc_out<RetCode> Port_Done;
-//     sc_inout_rv<32> Port_Data;
-//
-//     SC_CTOR(Memory)
-//     {
-//         SC_THREAD(execute);
-//         sensitive << Port_CLK.pos();
-//         dont_initialize();
-//
-//         m_data = new int[MEM_SIZE];
-//     }
-//
-//     ~Memory()
-//     {
-//         delete[] m_data;
-//     }
-//
-// private:
-//     int* m_data;
-//
-//     void execute()
-//     {
-//         while (true)
-//         {
-//             wait(Port_Func.value_changed_event());	// this is fine since we use sc_buffer
-//
-//             Function f = Port_Func.read();
-//             int addr   = Port_Addr.read();
-//             int data   = 0;
-//             if (f == FUNC_WRITE)
-//             {
-//                 cout << sc_time_stamp() << ": MEM received write" << endl;
-//                 data = Port_Data.read().to_int();
-//             }
-//             else
-//             {
-//                 cout << sc_time_stamp() << ": MEM received read" << endl;
-//             }
-//
-//             // This simulates memory read/write delay
-//             wait(99);
-//
-//             if (f == FUNC_READ)
-//             {
-//                 Port_Data.write( (addr < MEM_SIZE) ? m_data[addr] : 0 );
-//                 Port_Done.write( RET_READ_DONE );
-//                 wait();
-//                 Port_Data.write("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
-//             }
-//             else
-//             {
-//                 if (addr < MEM_SIZE)
-//                 {
-//                     m_data[addr] = data;
-//                 }
-//                 Port_Done.write( RET_WRITE_DONE );
-//             }
-//         }
-//     }
-// };
-
 
 
 SC_MODULE(Cache)
 {
 public:
-    sc_in<bool>       Port_CLK;
-    sc_in<Function>   Port_CpuFunc;
-    sc_in<int>        Port_CpuAddr;
-    sc_out<RetCode>   Port_CpuDone;
-    sc_inout_rv<32>   Port_CpuData;
+  sc_in<bool>       Port_CLK;
+  sc_in<Function>   Port_CpuFunc;
+  sc_in<int>        Port_CpuAddr;
+  sc_out<RetCode>   Port_CpuDone;
+  sc_inout_rv<32>   Port_CpuData;
 
-    // sc_in<RetCode>    Port_MemDone;
-    // sc_out<Function>  Port_MemFunc;
-    // sc_out<int>       Port_MemAddr;
-    // sc_inout_rv<32>   Port_MemData;
+  class Line {
+  public:
+    bool  isValid;
+    int   tag;
+    int   data;
 
-    class Line {
-      public:
-        bool  isValid;
-        int   tag;
-        int   data;
-
-        Line() {
-          isValid = false;
-          tag = 0;
-          data = 0;
-        }
-    };
-
-    class Set {
-      public:
-        bool linePresent;
-        Line line[NUM_LINES];
-        Set (){
-          linePresent = false;
-        };
-
-        int findTag (int tag){
-          for(int i=0 ; i<NUM_LINES ; i++){
-            if (line[i].tag == tag){
-              return i;
-            }
-          }
-          return -1;
-        }
-
-        // Shift all lines right up to the linePosition.
-        // If linePosition is LRU_POSITION then the entry is removed.
-        void shiftLines(int linePosition) {
-          for(int i = 0 ; i < linePosition ; i++) {
-            line[i+1] = line[i];
-          }
-        }
-    };
-
-    SC_CTOR(Cache)
-    {
-        Set set[NUM_SETS];
-        for (int i=0 ; i<NUM_SETS ; i++){
-          for (int j=0 ; j<NUM_LINES ; j++){
-            Line newLine;
-            set[i].line[j] = newLine;
-          }
-        }
-        cout << "cache constructed" << endl;
-        SC_THREAD(execute);
-        sensitive << Port_CLK.pos();
-        dont_initialize();
+    Line() {
+      isValid = false;
+      tag = 0;
+      data = 0;
     }
+  };
+
+  class Set {
+  public:
+    int numOfEntries;
+    Line line[NUM_LINES];
+    Set (){
+      numOfEntries = 0;
+    };
+
+    int findTag (int tag) {
+      for(int i=0 ; i<NUM_LINES ; i++){
+        if (line[i].tag == tag){
+          return i;
+        }
+      }
+      return -1;
+    }
+
+    // Shift all lines right up to the added line position. Remove from the LRU position.
+    void shiftLinesMiss(int numOfEntries) {
+      for(int i = numOfEntries; i > 1 ; i--) {
+        line[i-1] = line[i-2];
+      }
+    }
+    void shiftLinesHit(int linePosition) {
+      for(int i = 0; i < linePosition-1 ; i++) {
+        line[i+1] = line[i];
+      }
+    }
+
+    void reorderMiss(int numOfEntries, int tag, int data) {
+      if (numOfEntries == 1){
+        line[1] = line[0];
+      } else {
+        shiftLinesMiss(numOfEntries);
+      }
+      line[MRU_POSITION].isValid = true;
+      line[MRU_POSITION].tag = tag;
+      line[MRU_POSITION].data = data;
+      if (this->numOfEntries < NUM_LINES)
+        this->numOfEntries++;
+    }
+
+    void reorderHit(int linePosition) {
+      if (linePosition == 0) {
+        ;
+      } else if (linePosition == 1) {
+        swap(line[MRU_POSITION], line [linePosition]);
+      } else {
+        Line temp = line[linePosition];
+        shiftLinesHit(linePosition);
+        line[MRU_POSITION] = temp;
+      }
+    }
+  };
+
+  SC_CTOR(Cache)
+  {
+    SC_THREAD(execute);
+    sensitive << Port_CLK.pos();
+    dont_initialize();
+  }
 
 private:
-    // Set* set;
 
-    int getIndex (int address) {
-      return address & 0xFE0;
+  int getIndex (int address) {
+    return (address & 0x0000FE0) >> 5;
+  }
+
+  int getTag (int address) {
+    return (address & 0xFFFFF000) >> 12;
+  }
+
+  void execute()
+  {
+    Set set[NUM_SETS];
+    for (int i=0 ; i<NUM_SETS ; i++){
+      for (int j=0 ; j<NUM_LINES ; j++){
+        Line newLine;
+        set[i].line[j] = newLine;
+      }
     }
 
-    int getTag (int address) {
-      return address & 0xFFFFF000;
-    }
-
-    void execute()
+    while (true)
     {
-        while (true)
-        {
-            wait(Port_CpuFunc.value_changed_event());	// this is fine since we use sc_buffer
+      wait(Port_CpuFunc.value_changed_event());	// this is fine since we use sc_buffer
 
-            Function f = Port_CpuFunc.read();
-            int addr   = Port_CpuAddr.read();
-            // Port_MemAddr.write(addr);
-            // Port_MemFunc.write(f);
+      Function f = Port_CpuFunc.read();
+      int addr   = Port_CpuAddr.read();
+      int index  = getIndex(addr);
+      int tag    = getTag(addr);
+      int data   = 0;
 
-            int index  = getIndex(addr);
-            int tag    = getTag(addr);
-            int data   = 0;
+      cout << "Index: " << index << "   Tag: " << tag << endl;
 
-            Set currentSet = set[index];
+      int linePosition = set[index].findTag(tag);
+      int numOfEntries = set[index].numOfEntries;
 
-            int linePosition = currentSet.findTag(tag);
+      // cout << "linePosition: " << linePosition << endl;
+      // cout << "numOfEntries: " << numOfEntries << endl;
 
-            if (linePosition > -1)
-            currentSet.linePresent = true;
+      if (f == FUNC_WRITE)
+      {
+        cout << sc_time_stamp() << ": CACHE received write" << endl;
+        data = Port_CpuData.read().to_int();
 
-            if (f == FUNC_WRITE)
-            {
-                cout << sc_time_stamp() << ": CACHE received write" << endl;
-                data = Port_CpuData.read().to_int();
-            }
-            else
-            {
-                cout << sc_time_stamp() << ": CACHE received read" << endl;
-            }
+      }
+      else
+      {
+        cout << sc_time_stamp() << ": CACHE received read" << endl;
+      }
 
-            if (f == FUNC_READ)
-            {
-              //cache read hit
-              if(currentSet.linePresent) {
-                Line temp = currentSet.line[linePosition];
-                currentSet.shiftLines(linePosition);
-                currentSet.line[MRU_POSITION] = temp;
-              }
-              // cache read miss
-              else {
-                wait(100); // simulate memory access penalty
+      if (f == FUNC_READ)
+      {
+        if (linePosition > -1) {
+          cout << "READ HIT" << endl;
+          stats_readhit(0);
 
-                // data = Port_MemData.read().to_int();
-                currentSet.shiftLines(LRU_POSITION);
-                currentSet.line[MRU_POSITION].data = data;
-                currentSet.line[MRU_POSITION].isValid = true;
-              }
-                Port_CpuDone.write( RET_READ_DONE );
+          set[index].reorderHit(linePosition);
 
-            }
-            else //writing
-            {
-                // cache write hit
-                if (currentSet.linePresent) {
-                  ;
-                }
-                // cache write miss
-                else {
-                  if (currentSet.line[LRU_POSITION].isValid) {
-                    wait(100);
-                    // Port_MemData.write(data);
-                  }
-
-                  currentSet.shiftLines(LRU_POSITION);
-                  currentSet.line[MRU_POSITION].data = data;
-                  currentSet.line[MRU_POSITION].isValid = true;
-                }
-
-                // Port_CpuDone.write( RET_WRITE_DONE );
-            }
         }
+        else {
+          cout << "READ MISS" << endl;
+          stats_readmiss(0);
+          wait(100); // simulate memory access penalty
+
+          set[index].reorderMiss(numOfEntries, tag, data);
+        }
+
+        Port_CpuDone.write( RET_READ_DONE );
+
+      }
+      else //writing
+      {
+        if (linePosition > -1) {
+          cout << "WRITE HIT" << endl;
+          stats_writehit(0);
+
+          set[index].reorderHit(linePosition);
+        }
+        else {
+          cout << "WRITE MISS" << endl;
+          stats_writemiss(0);
+
+          if (set[index].line[LRU_POSITION].isValid) {
+            wait(100); // writeback
+          }
+
+          set[index].reorderMiss(numOfEntries, tag, data);
+        }
+        wait();
+        Port_CpuDone.write( RET_WRITE_DONE );
+      }
+
+      for (int i=0 ; i<NUM_LINES ; i++) {
+          cout << "Line : " << i << "   Tag: " << set[index].line[i].tag << "   Valid: " << set[index].line[MRU_POSITION].isValid << endl;
+      }
+
     }
+  }
 };
+
 
 SC_MODULE(CPU)
 {
 
 public:
-    sc_in<bool>                 Port_CLK;
-    sc_in<RetCode>              Port_CacheDone;
-    sc_out<Function>            Port_CacheFunc;
-    sc_out<int>                 Port_CacheAddr;
-    sc_inout_rv<32>             Port_CacheData;
+  sc_in<bool>                 Port_CLK;
+  sc_in<RetCode>              Port_CacheDone;
+  sc_out<Function>            Port_CacheFunc;
+  sc_out<int>                 Port_CacheAddr;
+  sc_inout_rv<32>             Port_CacheData;
 
-    SC_CTOR(CPU)
-    {
-        SC_THREAD(execute);
-        sensitive << Port_CLK.pos();
-        dont_initialize();
-    }
+  SC_CTOR(CPU)
+  {
+    SC_THREAD(execute);
+    sensitive << Port_CLK.pos();
+    dont_initialize();
+  }
 
 private:
-    void execute()
+  void execute()
+  {
+    TraceFile::Entry    tr_data;
+    Function  f;
+
+    // Loop until end of tracefile
+    while(!tracefile_ptr->eof())
     {
-        TraceFile::Entry    tr_data;
-        Function  f;
+      // Get the next action for the processor in the trace
+      if(!tracefile_ptr->next(0, tr_data))
+      {
+        cerr << "Error reading trace for CPU" << endl;
+        break;
+      }
 
-        // Loop until end of tracefile
-        while(!tracefile_ptr->eof())
+      switch(tr_data.type)
+      {
+        case TraceFile::ENTRY_TYPE_READ:
+        f = FUNC_READ;
+        break;
+
+        case TraceFile::ENTRY_TYPE_WRITE:
+        f = FUNC_WRITE;
+        break;
+
+        case TraceFile::ENTRY_TYPE_NOP:
+        break;
+
+        default:
+        cerr << "Error, got invalid data from Trace" << endl;
+        exit(0);
+      }
+
+      if(tr_data.type != TraceFile::ENTRY_TYPE_NOP)
+      {
+        Port_CacheAddr.write(tr_data.addr);
+        Port_CacheFunc.write(f);
+
+        if (f == FUNC_WRITE)
         {
-            // Get the next action for the processor in the trace
-            if(!tracefile_ptr->next(0, tr_data))
-            {
-                cerr << "Error reading trace for CPU" << endl;
-                break;
-            }
+          cout << sc_time_stamp() << ": CPU sends write" << endl;
 
-            // To demonstrate the statistic functions, we generate a 50%
-            // probability of a 'hit' or 'miss', and call the statistic
-            // functions below
-            int j = rand()%2;
-
-            switch(tr_data.type)
-            {
-                case TraceFile::ENTRY_TYPE_READ:
-                    f = FUNC_READ;
-                    if(j)
-                        stats_readhit(0);
-                    else
-                        stats_readmiss(0);
-                    break;
-
-                case TraceFile::ENTRY_TYPE_WRITE:
-                    f = FUNC_WRITE;
-                    if(j)
-                        stats_writehit(0);
-                    else
-                        stats_writemiss(0);
-                    break;
-
-                case TraceFile::ENTRY_TYPE_NOP:
-                    break;
-
-                default:
-                    cerr << "Error, got invalid data from Trace" << endl;
-                    exit(0);
-            }
-
-            if(tr_data.type != TraceFile::ENTRY_TYPE_NOP)
-            {
-                Port_CacheAddr.write(tr_data.addr);
-                Port_CacheFunc.write(f);
-
-                if (f == FUNC_WRITE)
-                {
-                    cout << sc_time_stamp() << ": CPU sends write" << endl;
-
-                    uint32_t data = rand();
-                    Port_CacheData.write(data);
-                    wait();
-                    Port_CacheData.write("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
-                }
-                else
-                {
-                    cout << sc_time_stamp() << ": CPU sends read" << endl;
-                }
-
-                wait(Port_CacheDone.value_changed_event());
-
-                if (f == FUNC_READ)
-                {
-                    cout << sc_time_stamp() << ": CPU reads: " << Port_CacheData.read() << endl;
-                }
-            }
-            else
-            {
-                cout << sc_time_stamp() << ": CPU executes NOP" << endl;
-            }
-            // Advance one cycle in simulated time
-            wait();
+          uint32_t data = rand();
+          Port_CacheData.write(data);
+          wait();
+          // Port_CacheData.write("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
+        }
+        else
+        {
+          cout << sc_time_stamp() << ": CPU sends read" << endl;
         }
 
-        // Finished the Tracefile, now stop the simulation
-        sc_stop();
+        // cout << "waiting" << endl;
+        wait(Port_CacheDone.value_changed_event());
+        // cout << "value changed" << endl;
+
+        if (f == FUNC_READ)
+        {
+          cout << sc_time_stamp() << ": CPU reads: " << Port_CacheData.read() << endl;
+        }
+      }
+      else
+      {
+        cout << sc_time_stamp() << ": CPU executes NOP" << endl;
+      }
+      // Advance one cycle in simulated time
+      wait();
+      cout << endl;
     }
+
+    // Finished the Tracefile, now stop the simulation
+    sc_stop();
+  }
 };
 
 
 int sc_main(int argc, char* argv[])
 {
-    try
-    {
-        // Get the tracefile argument and create Tracefile object
-        // This function sets tracefile_ptr and num_cpus
-        init_tracefile(&argc, &argv);
+  try
+  {
+    // Get the tracefile argument and create Tracefile object
+    // This function sets tracefile_ptr and num_cpus
+    init_tracefile(&argc, &argv);
 
-        // Initialize statistics counters
-        stats_init();
+    // Initialize statistics counters
+    stats_init();
 
-        // Instantiate Modules
-        CPU    cpu("cpu");
-        Cache  cache("cache");
-        // Memory mem("main_memory");
+    // Instantiate Modules
+    CPU    cpu("cpu");
+    Cache  cache("cache");
 
+    // Signals
+    sc_buffer<Function>   sigCpuFunc;
+    sc_buffer<RetCode>    sigCpuDone;
+    sc_signal<int>        sigCpuAddr;
+    sc_signal_rv<32>      sigCpuData;
 
-        // Signals
+    // The clock that will drive the CPU and Cache
+    sc_clock clk;
 
-        // signals cpu <=> cache
-        sc_buffer<Function>   sigCpuFunc;
-        sc_buffer<RetCode>    sigCpuDone;
-        sc_signal<int>        sigCpuAddr;
-        sc_signal_rv<32>      sigCpuData;
+    // Connecting module ports with signals
+    cache.Port_CpuFunc(sigCpuFunc);
+    cache.Port_CpuAddr(sigCpuAddr);
+    cache.Port_CpuData(sigCpuData);
+    cache.Port_CpuDone(sigCpuDone);
 
-        //signals cache <=> mem
-        // sc_buffer<Function>   sigMemFunc;
-        // sc_buffer<RetCode>    sigMemDone;
-        // sc_signal<int>        sigMemAddr;
-        // sc_signal_rv<32>      sigMemData;
+    cpu.Port_CacheFunc(sigCpuFunc);
+    cpu.Port_CacheAddr(sigCpuAddr);
+    cpu.Port_CacheData(sigCpuData);
+    cpu.Port_CacheDone(sigCpuDone);
 
-        // The clock that will drive the CPU, Cache and Memory
-        sc_clock clk;
+    cpu.Port_CLK(clk);
+    cache.Port_CLK(clk);
 
-        // Connecting module ports with signals
+    cout << "Running (press CTRL+C to interrupt)... " << endl;
 
-        // mem.Port_Func(sigMemFunc);
-        // mem.Port_Addr(sigMemAddr);
-        // mem.Port_Data(sigMemData);
-        // mem.Port_Done(sigMemDone);
-        //
-        // cache.Port_MemFunc(sigMemFunc);
-        // cache.Port_MemAddr(sigMemAddr);
-        // cache.Port_MemData(sigMemData);
-        // cache.Port_MemDone(sigMemDone);
+    // Start Simulation
+    sc_start();
 
-        cache.Port_CpuFunc(sigCpuFunc);
-        cache.Port_CpuAddr(sigCpuAddr);
-        cache.Port_CpuData(sigCpuData);
-        cache.Port_CpuDone(sigCpuDone);
+    // Print statistics after simulation finished
+    stats_print();
+  }
 
-        cpu.Port_CacheFunc(sigCpuFunc);
-        cpu.Port_CacheAddr(sigCpuAddr);
-        cpu.Port_CacheData(sigCpuData);
-        cpu.Port_CacheDone(sigCpuDone);
+  catch (exception& e)
+  {
+    cerr << e.what() << endl;
+  }
 
-        cpu.Port_CLK(clk);
-        cache.Port_CLK(clk);
-        // mem.Port_CLK(clk);
-
-
-        cout << "Running (press CTRL+C to interrupt)... " << endl;
-
-
-        // Start Simulation
-        sc_start();
-
-        // Print statistics after simulation finished
-        stats_print();
-    }
-
-    catch (exception& e)
-    {
-        cerr << e.what() << endl;
-    }
-
-    return 0;
+  return 0;
 }
